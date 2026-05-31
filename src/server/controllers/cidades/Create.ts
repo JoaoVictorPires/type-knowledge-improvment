292337e955dadc0d69a99f03cf6ad1a7b5ccdedb
JoaoVictorPires <joaovictorsantos0213@gmail.com>
2026-05-31 11:13:27 -0300

    Adiciona validação de dados para criação de cidades utilizando yup

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,17 +1,43 @@
 import { Request, Response } from "express";
-
+import * as y from 'yup';
 
 
 interface Icidade {
   nome: string;
+  cep: number;
+  estado: string;
 }
 
 
+const bodyValidation: y.ObjectSchema<Icidade> = y.object().shape({
+  nome: y.string().required().min(3),
+  cep: y.number().required().positive().integer(),
+  estado: y.string().required().min(2).max(2),
+});
 
-export const create = (req: Request<{}, {}, Icidade>, res: Response) =>{
 
-  const data: Icidade = req.body;
-  console.log(data);
+export const create = async (req: Request<{}, {}, Icidade>, res: Response) => {
+  let validatedData: Icidade | undefined = undefined;
+  try {
+    validatedData = await bodyValidation.validate(req.body);
+  } catch (err) {
+    const yError = err as y.ValidationError;
 
-  return res.send(data);
+    return res.json({
+      errors: {
+        default: yError.message,
+      }
+    });
+  }
+
+
+  console.log(validatedData);
+
+
+
+
+
+
+
+  return res.send('Create');
 };
